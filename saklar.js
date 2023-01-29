7291e25d4705963826af1455c17eb74cedb6753a
sukacode <firmansahalhadi@gmail.com>
2023-01-29 14:12:00 +0700

    update-js

--- saklar.js
+++ saklar.js
@@ -1,16 +1,24 @@
-function saklar(params) {
-  console.log(params);
-
+function saklar(lamp) {
+  let toggle1 = document.getElementById("default-toggle1");
+  let toggle2 = document.getElementById("default-toggle2");
+  let toggle3 = document.getElementById("default-toggle3");
   let lampu1 = document.getElementById("lampu1");
+  let lampu2 = document.getElementById("lampu2");
+  let lampu3 = document.getElementById("lampu3");
 
-  if (params == "on") {
-    //nyala
+  if (toggle1.checked) {
     lampu1.src = "assets/images/on.gif";
-  }
-  if (params == "off") {
-    //mati
+  } else {
     lampu1.src = "assets/images/off.gif";
   }
-
-  return lampu1;
+  if (toggle2.checked) {
+    lampu2.src = "assets/images/on.gif";
+  } else {
+    lampu2.src = "assets/images/off.gif";
+  }
+  if (toggle3.checked) {
+    lampu3.src = "assets/images/on.gif";
+  } else {
+    lampu3.src = "assets/images/off.gif";
+  }
 }
